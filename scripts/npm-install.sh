a8514ccb5f5c6b97900409798a9804a401aa9f7d
TheKhaleesi <mrunal.ghorpade123@gmail.com>
2020-03-09 23:45:12 -0400

    webapp included

--- scripts/npm-install.sh
+++ scripts/npm-install.sh
@@ -2,7 +2,7 @@
 cd /home/ubuntu
 cd ..
 cd ..
-cd etc/profile.d
-cp custom.sh /home/ubuntu/.env
-cd /home/ubuntu
+cd /etc/profile.d
+cp custom.sh /home/ubuntu/webapp/.env
+cd /home/ubuntu/webapp
 npm install
