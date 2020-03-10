6ab53f7d948a74beca309bb17187d0001f2824c5
TheKhaleesi <mrunal.ghorpade123@gmail.com>
2020-03-10 15:17:02 -0400

    given right to custom.sh

--- scripts/npm-install.sh
+++ scripts/npm-install.sh
@@ -5,7 +5,8 @@ cd ..
 cd /etc/profile.d
 sudo cp custom.sh /home/ubuntu
 cd /home/ubuntu
-sudo chmod 666 .env
+sudo chomd 777 custom.sh
+sudo chmod 777 .env
 sudo cat custom.sh >> .env
 printenv
 npm install
