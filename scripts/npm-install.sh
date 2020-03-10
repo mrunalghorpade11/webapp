05767a614432ddb6578694252cf8664c9641fd87
TheKhaleesi <mrunal.ghorpade123@gmail.com>
2020-03-09 23:18:43 -0400

    env try, s3 upload in webapp folder

--- scripts/npm-install.sh
+++ scripts/npm-install.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 cd /home/ubuntu
+cd ..
+cd ..
+cd etc/profile.d
+cp custom.sh /home/ubuntu/.env
+cd /home/ubuntu
 npm install
--- scripts/npm-start.sh
+++ scripts/npm-start.sh
@@ -1,8 +1,2 @@
 #!/bin/bash
-cd /home/ubuntu
-cd ..
-cd ..
-cd etc/profile.d
-cp custom.sh /home/ubuntu/.env
-cd /home/ubuntu
 npm start
