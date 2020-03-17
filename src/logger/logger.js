7421befcd7f5f71b717ddf1e3442111710d97e9d
TheKhaleesi <mrunal.ghorpade123@gmail.com>
2020-03-17 19:29:53 -0400

    npm app-root-path

--- src/logger/logger.js
+++ src/logger/logger.js
@@ -7,6 +7,7 @@
  */
 const log4js = require('log4js');
 const path = require("path");
+var appRoot = require('app-root-path');
 log4js.configure({
     appenders: {
         out: {
@@ -14,7 +15,7 @@ log4js.configure({
         },
         app: {
             type: 'file',
-            filename: `${path.resolve('../../')}/app.log`,
+            filename: `${appRoot}/app.log`,
             maxLogSize: 10485760,
             backups: 1,
             compress: true
