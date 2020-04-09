d4aaffd54db58bda2d0dd44a9e461fe11f5a8e73
TheKhaleesi <mrunal.ghorpade123@gmail.com>
2020-04-09 09:43:41 -0400

    certificate added in circle ci

--- src/modules/applicationPropertiesSingleton.js
+++ src/modules/applicationPropertiesSingleton.js
@@ -11,11 +11,17 @@ const port = {
 const contextPath = {
     contextPath: "/assignment"
 }
+const fs = require('fs');
+const rdsCa = fs.readFileSync('rds-ca-2019-root.pem');
 var Sequelize = require('sequelize');
 var sequelize = new Sequelize(process.env.DBNAME,process.env.DB_USERNAME,process.env.DB_PASSWORD,
     {
         host: process.env.DB_HOST,
-        dialect: 'mariadb'
+        dialect: 'mariadb',
+        ssl: {
+            rejectUnauthorized: true,
+            ca: [rdsCa]
+        }
     }
 );
 module.exports = {
